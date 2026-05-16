2f884f1115d591668c3d33d34d9e49e5ce142300
coltergeist.xp <cbubbico@pratt.edu>
2026-05-15 20:36:51 -0400

    Revert "merge issues!"
    This reverts commit 2467af57bb70f818b3c7f9c3c2bdea33d628dbb7.

--- Project 3 Physics Game/Assets/Scripts/AddDecal.cs
+++ Project 3 Physics Game/Assets/Scripts/AddDecal.cs
@@ -22,25 +22,16 @@ public class AddDecal : MonoBehaviour
         GameObject thisText = Instantiate(add, Vector3.zero, Quaternion.identity);
         Debug.Log("Instantiated!");
         thisText.GetComponent<TextMeshProUGUI>().text = "+" + number;
-        thisText.transform.parent = canvas.transform;
+        thisText.transform.SetParent(canvas.transform);
         StartCoroutine(FloatUp(thisText));
         Destroy(thisText);
     }
 
     public IEnumerator FloatUp(GameObject thisObj)
-<<<<<<< HEAD
     { 
         for (float alpha = 1f; alpha >= 0; alpha -= 0.1f)
         {
             thisObj.GetComponent<TextMeshPro>().alpha -= alpha;
-=======
-    {
-        Color c = thisObj.GetComponent<Renderer>().material.color;
-        for (float alpha = 1f; alpha >= 0; alpha -= 0.1f)
-        {
-            c.a = alpha;
-            thisObj.GetComponent<Renderer>().material.color = c;
->>>>>>> parent of 7b513ab (Plahytested, tweaked pegs but they still spawn out of bounds :/)
             thisObj.GetComponent<Transform>().position += new Vector3(0, alpha * 10);
             yield return null;
         }
--- Project 3 Physics Game/Assets/Scripts/PegSpawner.cs
+++ Project 3 Physics Game/Assets/Scripts/PegSpawner.cs
@@ -6,11 +6,6 @@ public class PegSpawner : MonoBehaviour
     private int spawnNumber;
     //stored prefab for peg
     public GameObject pegPrefab;
-<<<<<<< HEAD
-=======
-    private Transform spawnCollider;
-    private Bounds spawnArea;
->>>>>>> parent of 7b513ab (Plahytested, tweaked pegs but they still spawn out of bounds :/)
 
     private void OnEnable()
     {
@@ -24,12 +19,13 @@ public class PegSpawner : MonoBehaviour
     public void SpawnPegs()
     {
         spawnNumber = Random.Range(12, 30);
-        spawnCollider = GetComponent<Transform>();
-        spawnArea = new Bounds(spawnCollider.position, spawnCollider.localScale);
-        Vector3 randPos;
+        Vector3 targetPos;
+        //float x;
+        float offsetX;
+        float offsetY;
+        float offsetZ;
         for (int i = spawnNumber; i >= 0; i--)
         {
-<<<<<<< HEAD
 
             //x = Random.Range(0f, 1f);
             offsetX = Random.Range(-10f, 10f);
@@ -38,11 +34,6 @@ public class PegSpawner : MonoBehaviour
             targetPos = new Vector3(offsetX,offsetY,offsetZ);
             GameObject thisPeg = Instantiate(pegPrefab, gameObject.transform.position, Quaternion.identity, gameObject.transform);
             thisPeg.transform.localPosition = targetPos;
-=======
-            randPos = new Vector3(Random.Range(spawnArea.min.x, spawnArea.max.x), Random.Range(spawnArea.min.y, spawnArea.max.y), Random.Range(spawnArea.min.z, spawnArea.max.z));
-            GameObject thisPeg = Instantiate(pegPrefab, randPos, Quaternion.identity);
-            thisPeg.GetComponent<Renderer>().material.color = Random.ColorHSV();
->>>>>>> parent of 7b513ab (Plahytested, tweaked pegs but they still spawn out of bounds :/)
         }
     }
 }
